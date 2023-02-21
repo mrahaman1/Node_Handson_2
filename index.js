e0aad6b8e33220ce633626e4b6a2f55f992a0ea5
mrahaman1 <mustafijur.rahaman.73586@gmail.com>
2023-02-21 22:54:39 +0530

    done

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const fs = require('fs')
 const path = require('path')
 
 app.get('/', (req,res)=>{
-    res.send('I am on the home page')
+    res.send("<h1>I am on the home page</h1>")
     res.end();
 })
 
 app.get('/gallery', (req,res)=>{
-    res.send('I am on the Gallery page')
+    res.send("<h1>I am on the Gallery page</h1>")
     res.end();
 })
 
-// app.get('/submit', (req,res)=>{
-    // res.send('Get of Submit......')
-    // res.end();
-// })
-
+app.get('/submit', (req,res)=>{
+    res.send("<h1>Get of Submit......</h1>")
+    res.end();
+})
+// 
 // app.post("/submit", (req,res)=>{
     //  const content= "hello";
     // //  fs.appendFile(path.join(__dirname, "/massage.text"),content,function(err){
@@ -31,7 +31,7 @@ app.get('/gallery', (req,res)=>{
 
 
 app.post('/submit', (req,res)=>{
-    res.send('Post Worked......')
+    res.send("<h1>Post Worked......</h1>")
     res.end();
 })
 
